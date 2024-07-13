d18d04bc84782ae6e504b87b4a00ae856fd3d1aa
b4 <arch_n3m3s1s_000@protonmail.com>
2024-07-13 11:44:33 -0600

    saves

---
+++ src/Imports.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+import "solmate/tokens/ERC20.sol";
+
+pragma solidity 0.8.20;
+ 
+ contract Token is ERC20("name", "symbol", 18)
+ {
+
+ }
+
+ import "@openzeppelin/contracts/access/Ownable.sol";
+
+ contract TestOz is Ownable(msg.sender)
+ {
+
+ }
