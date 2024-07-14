338a819db226838567c609ba463ccbfae2332996
b4 <arch_n3m3s1s_000@protonmail.com>
2024-07-14 13:10:53 -0600

    console testing

--- test/Consolet.t.sol
+++ test/Consolet.t.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 pragma solidity 0.8.20;
 
 contract ConsoleTest is Test {
-    function testLogSomething() public {
-        console.log("Log something here", 123);
+    function testLogSomething() public  pure {
+        console.log("Log something here", uint(100000));
     }
 }
